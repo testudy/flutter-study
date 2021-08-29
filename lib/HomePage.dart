1b958678fe4bda9b2448aae6f17abd6062bd7641
胡继伟 <hu_jiwei@foxmail.com>
2021-08-29 20:35:01 +0800

    feat: 添加RandomWords类

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './RandomWords.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -14,7 +13,7 @@ class HomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: Center(
-        child: Text(generateWordPairs().first.asPascalCase),
+        child: RandomWords(),
       ),
     );
   }
